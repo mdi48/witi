5259f4128dea891f28d4e7b019759597f7de3202
Max <max.dickson2000@gmail.com>
2026-02-05 16:07:00 +1300

    need to extract the metadata from the desc file. will write a function for this next.

--- main.go
+++ main.go
@@ -25,9 +25,21 @@ func main() {
 
 	pkgName := os.Args[1]
 	fmt.Println("Fetching package information for:", pkgName)
+	fmt.Println(findPackageDir(pkgName)) // Testing (it works!)
 }
 
-// IMPLEMENT HERE: function to get package info (makes call to findPackageDir)
+// gets package info from pacman's local database
+func getPackageInfo(pkgName string) (*Package, error) {
+	// find the package dir (format: pkgname-version)
+	pkgDir, err := findPackageDir(pkgName)
+	if err != nil {
+		return nil, err
+	}
+
+	// read the desc file in the package dir
+	descFile := filepath.Join(pkgDir, "desc")
+	// Need to parse the desc file now
+}
 
 // finds the package director in /var/lib/pacman/local/
 func findPackageDir(pkgName string) (string, error) {
@@ -44,3 +56,5 @@ func findPackageDir(pkgName string) (string, error) {
 
 	return "", fmt.Errorf("package directory not found for package: %s", pkgName)
 }
+
+// IMPLEMENT HERE: function to parse the desc file so we can extract package info
